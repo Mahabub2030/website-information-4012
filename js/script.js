fa0e14b776d37e08deb7a9f2741f3a37cd3b8f4d
mahabub <mahabubalom72091@gmail.com>
2024-05-20 12:10:22 +0300

    adding

--- js/script.js
+++ js/script.js
@@ -1,11 +1,41 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
+//     var table = $('#example').DataTable({
+
+//         buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+//     });
+
+//     table.buttons().container()
+//     .appendTo('#example_wrapper .col-md-6:eq(0)');
+// });
+
+
+($document).ready(function() {
     var table = $('#example').DataTable({
-
-        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        buttons: ['copy', 'csv', 'excel', 'pdf', 'print', 'add', 'remove'],
+        columnDefs: [
+            // Define column rendering for the C4 column
+            {
+                targets: 3, // Index of the C4 column (zero-based)
+                render: function(data, type, row, meta) {
+                    // Parse the expiration date from the data
+                    var expirationDate = new Date(data);
+                    // Get the current date
+                    var currentDate = new Date();
+                    // Check if expiration date is near (within 7 days)
+                    if (expirationDate - currentDate < 7 * 24 * 60 * 60 * 1000 && expirationDate - currentDate > 0) {
+                        // Return the data with a CSS class for highlighting
+                        return '<div class="near-expiry">' + data + '</div>';
+                    } else {
+                        // Return the data as is
+                        return data;
+                    }
+                }
+            }
+        ]
     });
 
-    table.buttons().container()
-    .appendTo('#example_wrapper .col-md-6:eq(0)');
+    // Move buttons container to desired location
+    table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
 });
     
